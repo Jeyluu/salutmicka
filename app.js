cf920b996b43a81046d3fb6154585c3632e2cc84
Jeyluu <73641462+Jeyluu@users.noreply.github.com>
2021-02-09 10:41:33 +0100

    structuration

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+// EJS
+app.set('view engine', 'ejs');
+
+
+//DOSSIER STATIC PUBLIC
+app.use(express.static(path.join(__dirname,'public')));
+
+
+//ROUTE
 
 //CONTROLERS
 app.get('/', (req,res) => {
