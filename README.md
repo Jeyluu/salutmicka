dcb256dd6dd443ba3e34081dd88fbf3049e00a3f
Jeyluu <73641462+Jeyluu@users.noreply.github.com>
2021-02-09 12:04:31 +0100

    configure mysql et creation de la variable global

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 Procédure : 
 
 1. Créer la base de donnée avec la base de donnée SQL
+
+2. Créé un ficher .env et mettez à jour vos données de connection
+
+        DB_HOST=localhost
+        DB_USER=root
+        DB_PASS=
+        DB_NAME=
--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mysql = require('mysql');
+const util = require('util')
+
+
+// DotEnv
+require('dotenv').config()
+
+
+//MYSQL
+const db = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+
+    }
+)
+
+db.connect(
+    (err) => {
+        if (err) { throw err }
+        console.log('Connecté au serveur MySQL');
+    }
+)
+//DECLARE LA VARIABLE LOCAL QUERYSQL
+global.querysql = util.promisify(db.query).bind(db)
+
 
 // EJS
 app.set('view engine', 'ejs');
 
 
 //DOSSIER STATIC PUBLIC
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //ROUTE
@@ -18,7 +46,7 @@ app.use('/liste-des-articles', articles)
 
 
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Connecté')
 })
 
